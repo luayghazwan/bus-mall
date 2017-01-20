4463bdcbe8175a4f211201ea4c65ca04c108fffd
‘LuayYounus’ <luayghazwan@yahoo.com>
2017-01-19 23:51:01 -0800

    took some lines out for readibility and arranged my functions flow

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 'use strict';
 
 var counter = 0;
-var instance = [];
-var number; // our unique random number that is generated
-var currentArary = [];
-var historyArray = [];
+var maxClicks = 0; //this goes up to 25 clicks based on the condition at the bottom
+var number; // new random number
+var instances = [];
+var currentArary = []; // for 3 random numbers
+var historyArray = []; // get the 3 old numbers to check if they are in the array
 var localArray = [];
-var maxClicks = 0;
 var imagesChart;
+
 var chartColors = ['#800080','#FF00FF','#000080','#0000FF','#008080','#00FFFF','#008000','#00FF00','#FF0000','#808080','#800080','#FF00FF','#000080','#0000FF','#008080','#00FFFF','#008000','#00FF00','#FF0000','#808080'];
 var images = ['bag','banana','bathroom','boots','breakfast','bubblegum','chair','cthulhu','dog-duck','dragon','pen','pet-sweep','scissors','shark','sweep','tauntaun','unicorn','usb','water-can','wine-glass'];
 
@@ -24,15 +25,15 @@ function Image (name, path) {
   this.timesClicked = 0;
 };
 
-function instanceCreator() {
+//creating multiple instances from our object
+function instancesCreator() {
   for (var index = 0; index < images.length ; index++) {
-    instance[index] = new Image(images[index], 'img/' + images[index] + '.jpg');
+    instances[index] = new Image(images[index], 'img/' + images[index] + '.jpg');
   }
 }
+instancesCreator();
 
-instanceCreator();
-// console.log('instance',instance);
-
+//object that does the random number and return a non-duplicate number
 var sourcePhoto = {
   randomNumber: function() {
     number = Math.floor(Math.random() * images.length) + 0;
@@ -48,40 +49,40 @@ var sourcePhoto = {
       currentArary.push(number);
     }
 
-    img1.setAttribute('src', instance[currentArary[0]].path);
-    img2.setAttribute('src', instance[currentArary[1]].path);
-    img3.setAttribute('src', instance[currentArary[2]].path);
+    img1.setAttribute('src', instances[currentArary[0]].path);
+    img2.setAttribute('src', instances[currentArary[1]].path);
+    img3.setAttribute('src', instances[currentArary[2]].path);
   }
 };
 sourcePhoto.showImage();
 
+//adding event listeners to make our images clickable
 img1.addEventListener('click', photoFunction1,false);
 function photoFunction1() {
-  instance[currentArary[0]].timesClicked++;
+  instances[currentArary[0]].timesClicked++;
   maxClicks++;
-  // console.log(instance, 'iiiiiii');
+  // console.log(instances, 'iiiiiii');
   removeListener();
   sourcePhoto.showImage();
 }
-
 img2.addEventListener('click', photoFunction2,false);
 function photoFunction2() {
-  instance[currentArary[1]].timesClicked++;
+  instances[currentArary[1]].timesClicked++;
   maxClicks++;
   // console.log('max clicks' , maxClicks);
   removeListener();
   sourcePhoto.showImage();
 }
-
 img3.addEventListener('click', photoFunction3,false);
 function photoFunction3() {
   // console.log();
-  instance[currentArary[2]].timesClicked++;
+  instances[currentArary[2]].timesClicked++;
   maxClicks++;
   removeListener();
   sourcePhoto.showImage();
 }
 
+//removing listeners when max Clicks reached +  + Calling our LOCAL STORAGE
 function removeListener (){
   if (maxClicks === 25) {
     // console.log('inside if');
@@ -95,20 +96,17 @@ function removeListener (){
       console.log('empty local storage!');
       pushingToLocalStorage();
     } else {
-      for (var i = 0; i < instance.length ; i++) {
+      for (var i = 0; i < instances.length ; i++) {
         var temp = [];
         localStorage.key = JSON.parse('');
-        instance[i] = instance[i].timesClicked;
-        localStorage.key = JSON.stringify(instance);
+        instances[i] = instances[i].timesClicked;
+        localStorage.key = JSON.stringify(instances);
+        instances[i].timesClicked += oldLocalSto[i].timesClicked;
+        localStorage.key = JSON.stringify(instances);
       }
-    }
-
-    // if (localStorage.key) {
-    //   persistToLocalStorage();
-    // } else { localStorage.key = JSON.stringify(instance);
-    // };
-    showAllImages(); //to show all photos with their counts after no. of clicks
-    createChart();
+    };
+    showAllImages(); // calling to show all photos with their counts after the clicks
+    createChart(); //calling chart to draw the bars percentage per photo
   }
 }
 
@@ -116,24 +114,25 @@ function removeListener (){
 function showAllImages() {
   for (var i = 0; i < images.length; i++) {
     var div = document.createElement('div');
-    div.textContent = instance[i].name;
+    div.textContent = instances[i].name;
 
     var p = document.createElement('p');
-    p.textContent = 'Times Shown: ' + instance[i].timesShown + ' - Times Clicked: ' + instance[i].timesClicked ;
+    p.textContent = 'Times Shown: ' + instances[i].timesShown + ' - Times Clicked: ' + instances[i].timesClicked ;
     div.appendChild(p);
 
     var img = document.createElement('img');
-    img.setAttribute('src',instance[i].path);
+    img.setAttribute('src',instances[i].path);
     div.appendChild(img);
     imag.appendChild(div);
   }
 }
 
+//drawing a chart of our results
 function createChart () {
   var chartData = [];
-  for (var i = 0 ; i < instance.length; i++) {
-    chartData.push(instance[i].timesClicked);
-    // console.log(instance[i].timesClicked);
+  for (var i = 0 ; i < instances.length; i++) {
+    chartData.push(instances[i].timesClicked);
+    // console.log(instances[i].timesClicked);
   };
   var chartOptions = {
     scales: {
@@ -159,22 +158,9 @@ function createChart () {
   } );
 }
 
-// function persistToLocalStorage () {
-//   var oldLocalSto;
-//   oldLocalSto = JSON.parse(localStorage.key);
-//   for (var i = 0; i < images.length; i++) {
-//     instance[i].timesClicked += oldLocalSto[i].timesClicked;
-//     localStorage.key = JSON.stringify(instance);
-//   }
-// };
-  // localStorage['Counter'] = 0;
-  // var i = localStorage['Counter'];
-  // var d = localStorage['Value' + i];
-  // i = i + 1;
-  // var f = localStorage['Value' + i];
-
+//stores to local Storage
 function pushingToLocalStorage() {
-  for (var i = 0 ; i < instance.length ; i++){
+  for (var i = 0 ; i < instances.length ; i++){
 
   }
   localStorage.key = JSON.stringify(localArray);
